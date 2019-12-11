5a125415c920388149b49699b809332f55c246b7
erdalsehalji <58055108+erdalsehalji@users.noreply.github.com>
2019-12-11 08:19:23 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+
+  <div id="app">
+
+  
+      <div class="bg-white">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <nav class="navbar navbar-expand-lg navbar-light mb-2">
+                <router-link class="navbar-brand" to="/"></router-link>
+              </router-link>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+            
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <form class="form-inline my-2 my-lg-0 mr-auto ml-5">
+                  <input v-model="searchTerm" class="form-control mr-sm-2" type="search" placeholder="Trazi nesto" aria-label="Search">
+                </form>
+                <router-link v-if="!authenticated" class="btn btn-info my-2 my-sm-0 mr-2" to="/login">Login</router-link>
+                <span v-if="authenticated">
+                  {{ userName }}
+                  <a @click="logout" class="btn btn-info my-2 my-sm-0 mr-2" href="#">Logout</a>
+                </span>
+                <router-link class="btn btn-outline my-2 my-sm-0 mr-2" to="/signup">Signup</router-link>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+    </div>
+
+      <div class="container">
+ 
+        <router-view/>
+        
+
+        
+
+    </div>
+
+    <div id="footer" class="text-center mt-5">Ovo je footer. (c) 2019 mi svi skupa.</div>
+  </div>
+</template>
+
+<script type="text/javascript">
+
+
+export default {
+  data () {
+    return store;
+  },
+
+  methods: {
+    logout() {
+      store.authenticated = false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  background-color: rgba(var(--b3f,250,250,250),1)
+}
+
+.bg-white {
+  background-color: lightcoral;
+  border-bottom: lightcoral;
+  margin-bottom: 20px;
+  padding: 10px;
+}
+
+nav.navbar {
+  background-color: white;
+}
+</style>
---
+++ src/views/login.vue
@@ -0,0 +1,140 @@
+<style>
+  #log {
+    background-color: lightslategray;
+    padding-bottom: 10 px;
+    text-align: center;
+  }
+
+  #reg {
+    background-color: rgb(19, 227, 36);
+    text-align: center;
+  }
+
+  #dob {}
+</style>
+
+<template>
+
+  <div>
+
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <br>
+
+
+
+    <div class="container">
+      <div class="row">
+
+        <div id="raz" class="col-1">
+
+        </div>
+
+        <div id="log" class="col-3">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                placeholder="email">
+              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+
+        <div id="raz" class="col-1">
+
+        </div>
+
+        <div id="reg" class="col-6">
+
+          <div class="form-row">
+
+            <div class="form-group col-md-6">
+              <label for="inputEmail4">Ime</label>
+              <input type="email" class="form-control" id="inputEmail4" placeholder="ime">
+            </div>
+
+            <div class="form-group col-md-6">
+              <label for="inputEmail4">Prezime</label>
+              <input type="email" class="form-control" id="inputEmail4" placeholder="prezime">
+            </div>
+
+            <div class="form-group col-md-6">
+              <label for="inputEmail4">Email</label>
+              <input type="email" class="form-control" id="inputEmail4" placeholder="email">
+            </div>
+
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Password</label>
+              <input type="Password" id="inputpassword" placeholder="Password" class="form-control">
+            </div>
+
+
+            <div id="dob" class="form-group col-md-6">
+              <label for="inputState">Spol</label>
+              <select id="inputState" class="form-control">
+                <option selected>Odaberi...</option>
+                <option>M</option>
+                <option>Ž</option>
+              </select>
+            </div>
+
+            <div id="dob" class="form-group col-md-6">
+              <label for="inputState">Dob</label>
+              <select id="inputState" class="form-control">
+                <option selected>Odaberi...</option>
+                <option>1</option>
+                <option>...</option>
+              </select>
+            </div>
+
+          </div>
+          <button type="submit" class="btn btn-primary">Registracija</button>
+          <br>
+        </div>
+
+        </form>
+      </div>
+
+
+
+    </div>
+
+    <div id="raz" class="col-1">
+
+    </div>
+
+
+
+
+
+  </div>
+  </div>
+</template>
